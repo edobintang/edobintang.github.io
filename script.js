91f561d9c82fa81e25d29956723092c4d04fd42e
Edo Bintang <edo@Rizals-MacBook-Pro.local>
2020-11-20 16:10:16 +0700

    Edo-FinalProject0

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ function generatePassword() {
         const x = generateX()
         password += x
     }
-
+    console.log(password)
     pwElement.innerText = password
 }
 
@@ -80,7 +80,7 @@ function generateX() {
         return ''
     }
 
-    return result[Math.floor(Math.random() * xs.length)]
+    return result[Math.floor(Math.random() * result.length)]
 }
 
 generateElement.addEventListener('click', generatePassword)
@@ -99,4 +99,52 @@ copyElement.addEventListener('click', () => {
     textarea.remove()
     alert("Password copied to Clipboard!")
 })
+
+let c = document.getElementById("c");
+let ctx = c.getContext("2d");
+
+c.height = window.innerHeight;
+c.width = window.innerWidth;
+
+//Matrix characters - taken from the unicode charset
+let charMatrix = "eduardobintang";
+//converting the string into an array of single characters
+charMatrix = charMatrix.split("");
+
+const font_size = 10;
+let columns = c.width / font_size; //number of columns for the rain
+//an array of drops - one per column
+let drops = [];
+//x below is the x coordinate
+//1 = y co-ordinate of the drop(same for every drop initially)
+for (let x = 0; x < columns; x++)
+    drops[x] = 1;
+
+//drawing the characters
+function draw() {
+    //Black BG for the canvas
+    //translucent BG to show trail
+    ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
+    ctx.fillRect(0, 0, c.width, c.height);
+
+    ctx.fillStyle = "#0F0"; //green text
+    ctx.font = font_size + "px arial";
+    //looping over drops
+    for (let i = 0; i < drops.length; i++) {
+        //a random chinese character to print
+        let text = charMatrix[Math.floor(Math.random() * charMatrix.length)];
+        //x = i*font_size, y = value of drops[i]*font_size
+        ctx.fillText(text, i * font_size, drops[i] * font_size);
+
+        //sending the drop back to the top randomly after it has crossed the screen
+        //adding a randomness to the reset to make the drops scattered on the Y axis
+        if (drops[i] * font_size > c.height && Math.random() > 0.975)
+            drops[i] = 0;
+
+        //incrementing Y coordinate
+        drops[i]++;
+    }
+}
+
+setInterval(draw, 33);
 
